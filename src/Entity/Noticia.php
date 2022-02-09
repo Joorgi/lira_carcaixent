aeac106021d9c2e2bbac9515e8e0fb6e63a0aec4
joorgi <joorgi@floridauniversitaria.es>
2022-02-09 16:34:08 +0100

    Add Noticia entity

---
+++ src/Entity/Noticia.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NoticiaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NoticiaRepository::class)]
+class Noticia
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: 'ID', type: 'integer')]
+    private $id;
+
+    #[ORM\Column(name: 'TITULO', type: 'string', length: 50)]
+    private $titulo;
+
+    #[ORM\Column(name: 'FECHA', type: 'datetime')]
+    private $fecha;
+
+    #[ORM\Column(name: 'DESCRIPCION', type: 'string', length: 255)]
+    private $descripcion;
+
+    #[ORM\Column(name: 'IMAGEN', type: 'string', length: 255, nullable: true)]
+    private $imagen;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitulo(): ?string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): self
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getFecha(): ?\DateTimeInterface
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTimeInterface $fecha): self
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getImagen(): ?string
+    {
+        return $this->imagen;
+    }
+
+    public function setImagen(?string $imagen): self
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+}
