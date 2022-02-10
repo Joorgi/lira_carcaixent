781576e00d34bb0b2beaf8b5651949a8d70dface
joorgi <joorgi@floridauniversitaria.es>
2022-02-10 18:56:14 +0100

    Add Banda properties

--- src/Entity/Banda.php
+++ src/Entity/Banda.php
@@ -13,8 +13,23 @@ class Banda
     #[ORM\Column(type: 'integer')]
     private $id;
 
+    #[ORM\Column(type: 'string', length: 50)]
+    private $nombre;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
     }
 }
