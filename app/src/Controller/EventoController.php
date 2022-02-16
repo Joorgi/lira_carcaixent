ff2b2f988dcc33e88a0e789dbfd4ed21dc627cc1
joorgi <joorgi@floridauniversitaria.es>
2022-02-16 09:54:05 +0100

    add toString function

--- app/src/Controller/EventoController.php
+++ app/src/Controller/EventoController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('admin/evento')]
+#[Route('/admin/evento')]
 class EventoController extends AbstractController
 {
     #[Route('/all', name: 'evento_index', methods: ['GET'])]
