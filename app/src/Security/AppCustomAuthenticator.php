16aa2a6d9d798cf3d94ee77df228254f7246e21d
joorgi <joorgi@floridauniversitaria.es>
2022-02-17 14:05:28 +0100

    edit login and auth files

--- app/src/Security/AppCustomAuthenticator.php
+++ app/src/Security/AppCustomAuthenticator.php
@@ -2,30 +2,61 @@
 
 namespace App\Security;
 
-use App\Entity\User;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Http\Attribute\CurrentUser;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-#[Route('/api/')]
-class ApiLoginController extends AbstractController
+class Authenticator extends AbstractLoginFormAuthenticator
 {
-  #[Route('login', name: 'api_login', methods: ['POST'])]
-  public function index(?UserInterface $user): Response
+  use TargetPathTrait;
+
+  public const LOGIN_ROUTE = 'app_login';
+
+  private UrlGeneratorInterface $urlGenerator;
+
+  public function __construct(UrlGeneratorInterface $urlGenerator)
   {
-    if (null === $user) {
-      return $this->json([
-        'message' => 'missing credentials',
-      ], Response::HTTP_UNAUTHORIZED);
+    $this->urlGenerator = $urlGenerator;
+  }
+
+  public function authenticate(Request $request): Passport
+  {
+    $email = $request->request->get('email', '');
+
+    $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+    return new Passport(
+      new UserBadge($email),
+      new PasswordCredentials($request->request->get('password', '')),
+      [
+        new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+      ]
+    );
+  }
+
+  public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+  {
+    if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+      return new RedirectResponse($targetPath);
     }
 
+    // For example:
+    return new RedirectResponse($this->urlGenerator->generate('main'));
+    //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+  }
 
-    return $this->json([
-      'user'  => $user->getUserIdentifier(),
-      'roleUser' => $user->getRoles(),
-      'message' => "Login successful"
-    ]);
+  protected function getLoginUrl(Request $request): string
+  {
+    return $this->urlGenerator->generate(self::LOGIN_ROUTE);
   }
 }
