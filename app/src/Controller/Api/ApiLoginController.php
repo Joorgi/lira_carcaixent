68414463dea0d0453d9e227aa867022308a5ea89
joorgi <joorgi@floridauniversitaria.es>
2022-02-17 13:58:33 +0100

    security acces sensio success

--- app/src/Controller/Api/ApiLoginController.php
+++ app/src/Controller/Api/ApiLoginController.php
@@ -1,31 +1,31 @@
 <?php
 
-namespace App\Controller\Api;
+// namespace App\Controller\Api;
 
-use App\Entity\User;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\CurrentUser;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\User\UserInterface;
+// use App\Entity\User;
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Component\Security\Http\Attribute\CurrentUser;
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\Security\Core\User\UserInterface;
 
-#[Route('/api/')]
-class ApiLoginController extends AbstractController
-{
-  #[Route('login', name: 'api_login')]
-  public function index(?UserInterface $user): Response
-  {
-    if (null === $user) {
-      return $this->json([
-        'message' => 'missing credentials',
-      ], Response::HTTP_UNAUTHORIZED);
-    }
+// #[Route('/api/')]
+// class ApiLoginController extends AbstractController
+// {
+//   #[Route('login', name: 'api_login')]
+//   public function index(?UserInterface $user): Response
+//   {
+//     if (null === $user) {
+//       return $this->json([
+//         'message' => 'missing credentials',
+//       ], Response::HTTP_UNAUTHORIZED);
+//     }
 
 
-    return $this->json([
-      'user'  => $user->getUserIdentifier(),
-      'roleUser' => $user->getRoles(),
-      'message' => "Login successful"
-    ]);
-  }
-}
+//     return $this->json([
+//       'user'  => $user->getUserIdentifier(),
+//       'roleUser' => $user->getRoles(),
+//       'message' => "Login successful"
+//     ]);
+//   }
+// }
--- app/src/Security/AppCustomAuthenticator.php
+++ app/src/Security/AppCustomAuthenticator.php
@@ -5,26 +5,27 @@ namespace App\Security;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-// #[Route('/api/')]
-// class ApiLoginController extends AbstractController
-// {
-//     #[Route('login', name: 'api_login')]
-//     public function index(#[CurrentUser] ?User $user): Response
-//     {
-//         if (null === $user) {
-//             return $this->json([
-//                 'message' => 'missing credentials',
-//             ], Response::HTTP_UNAUTHORIZED);
-//         }
+#[Route('/api/')]
+class ApiLoginController extends AbstractController
+{
+  #[Route('login', name: 'api_login')]
+  public function index(?UserInterface $user): Response
+  {
+    if (null === $user) {
+      return $this->json([
+        'message' => 'missing credentials',
+      ], Response::HTTP_UNAUTHORIZED);
+    }
 
 
-//         return $this->json([
-//             'user'  => $user->getUserIdentifier(),
-//             'roleUser' => $user->getRoles(),
-//             'message' => "Login successful"
-//         ]);
-//     }
-// }
+    return $this->json([
+      'user'  => $user->getUserIdentifier(),
+      'roleUser' => $user->getRoles(),
+      'message' => "Login successful"
+    ]);
+  }
+}
