5acc2f23e057f5d7d561297fbc72a3f0ed268741
joorgi <joorgi@floridauniversitaria.es>
2022-02-18 08:51:17 +0100

    edit custom auth

--- app/src/Security/AppCustomAuthenticator.php
+++ app/src/Security/AppCustomAuthenticator.php
@@ -16,47 +16,47 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class Authenticator extends AbstractLoginFormAuthenticator
-{
-  use TargetPathTrait;
+// class Authenticator extends AbstractLoginFormAuthenticator
+// {
+//   use TargetPathTrait;
 
-  public const LOGIN_ROUTE = 'app_login';
+//   public const LOGIN_ROUTE = 'app_login';
 
-  private UrlGeneratorInterface $urlGenerator;
+//   private UrlGeneratorInterface $urlGenerator;
 
-  public function __construct(UrlGeneratorInterface $urlGenerator)
-  {
-    $this->urlGenerator = $urlGenerator;
-  }
+//   public function __construct(UrlGeneratorInterface $urlGenerator)
+//   {
+//     $this->urlGenerator = $urlGenerator;
+//   }
 
-  public function authenticate(Request $request): Passport
-  {
-    $email = $request->request->get('email', '');
+//   public function authenticate(Request $request): Passport
+//   {
+//     $email = $request->request->get('email', '');
 
-    $request->getSession()->set(Security::LAST_USERNAME, $email);
+//     $request->getSession()->set(Security::LAST_USERNAME, $email);
 
-    return new Passport(
-      new UserBadge($email),
-      new PasswordCredentials($request->request->get('password', '')),
-      [
-        new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
-      ]
-    );
-  }
+//     return new Passport(
+//       new UserBadge($email),
+//       new PasswordCredentials($request->request->get('password', '')),
+//       [
+//         new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+//       ]
+//     );
+//   }
 
-  public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
-  {
-    if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-      return new RedirectResponse($targetPath);
-    }
+//   public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+//   {
+//     if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+//       return new RedirectResponse($targetPath);
+//     }
 
-    // For example:
-    return new RedirectResponse($this->urlGenerator->generate('main'));
-    //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-  }
+//     // For example:
+//     return new RedirectResponse($this->urlGenerator->generate('main'));
+//     //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+//   }
 
-  protected function getLoginUrl(Request $request): string
-  {
-    return $this->urlGenerator->generate(self::LOGIN_ROUTE);
-  }
-}
+//   protected function getLoginUrl(Request $request): string
+//   {
+//     return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+//   }
+// }
